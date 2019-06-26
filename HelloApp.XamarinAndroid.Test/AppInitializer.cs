d03433cef790214cc2b0b6064503035d59034da8
Ayumu Inaba <ainaba@microsoft.com>
2019-06-26 16:55:10 +0900

    UITest

---
+++ HelloApp.XamarinAndroid.Test/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace HelloApp.XamarinAndroid.Test
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.HelloApp.XamarinAndroid").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
